b566bdc0de5b2d95d932d0dbe9155d8bd3f9659c
David Gomesh <davidgomesh2016@gmail.com>
2023-04-07 17:19:16 +0100

    Added Product Command

--- run-project.sh
+++ run-project.sh
@@ -10,6 +10,9 @@ git clone https://github.com/INSIS-s-Projects-ISEP/acme-discovery-system.git
 ECHO "Downloading Loading Balancer..."
 git clone https://github.com/INSIS-s-Projects-ISEP/acme-load-balancer.git
 
+ECHO "Downloading Product Command..."
+git clone https://github.com/INSIS-s-Projects-ISEP/acme-product-command.git
+
 ECHO "Running docker-compose.yml file..."
 docker compose up -d --build
 
