3043dafc57eada520f543969b407e3e9da9c3b4d
David Gomesh <davidgomesh2016@gmail.com>
2023-04-07 16:37:55 +0100

    Added docker-compose and shell script files

---
+++ run-project.sh
@@ -0,0 +1,16 @@
+
+ECHO OFF
+CLEAR
+
+ECHO "Hello, World!"
+
+ECHO "Downloading Discovery System..."
+git clone https://github.com/INSIS-s-Projects-ISEP/acme-discovery-system.git
+
+ECHO "Downloading Loading Balancer..."
+git clone https://github.com/INSIS-s-Projects-ISEP/acme-load-balancer.git
+
+ECHO "Running docker-compose.yml file..."
+docker compose up -d --build
+
+PAUSE
