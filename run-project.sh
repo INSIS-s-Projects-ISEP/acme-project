fe374d4097b743c848cdb1e5bd5e9101aa311f09
David Gomesh <davidgomesh2016@gmail.com>
2023-04-07 17:34:41 +0100

    Added Review and Vote (Command and Query)

--- run-project.sh
+++ run-project.sh
@@ -13,6 +13,21 @@ git clone https://github.com/INSIS-s-Projects-ISEP/acme-load-balancer.git
 ECHO "Downloading Product Command..."
 git clone https://github.com/INSIS-s-Projects-ISEP/acme-product-command.git
 
+# ECHO "Downloading Product Query..."
+# git clone https://github.com/INSIS-s-Projects-ISEP/acme-product-query.git
+
+ECHO "Downloading Review Command..."
+git clone https://github.com/INSIS-s-Projects-ISEP/acme-review-command.git
+
+ECHO "Downloading Review Query..."
+git clone https://github.com/INSIS-s-Projects-ISEP/acme-review-query.git
+
+ECHO "Downloading Vote Command..."
+git clone https://github.com/INSIS-s-Projects-ISEP/acme-vote-command.git
+
+ECHO "Downloading Vote Query..."
+git clone https://github.com/INSIS-s-Projects-ISEP/acme-vote-query.git
+
 ECHO "Running docker-compose.yml file..."
 docker compose up -d --build
 
